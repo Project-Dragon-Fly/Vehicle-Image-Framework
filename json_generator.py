9fa26f885dbeeff7d894f88b9fb31152995ea721
Edwin Jose George <edwinjosegeorge@gmail.com>
2022-04-26 21:15:41 +0530

    convert folder structure into json

---
+++ json_generator.py
@@ -0,0 +1,29 @@
+import os
+import json
+
+
+def process_folder(path, holder):
+    for file in os.listdir(path):
+        target = os.path.join(path, file)
+        if os.path.isdir(target):
+            child = dict()
+            child['name'] = file
+            child['children'] = list()
+            child['images'] = list()
+            child['type'] = 'images'
+            holder['type'] = 'folder'
+            holder["children"].append(child)
+            process_folder(target, child)
+        elif os.path.splitext(file)[1] in {'.png', 'jpg'}:
+            holder['images'].append(file)
+
+
+values = dict()
+values['name'] = 'Vehicles'
+values['children'] = list()
+values['images'] = list()
+values['type'] = 'images'
+process_folder('Vehicles', values)
+json_object = json.dumps(values, indent=4)
+with open('myfile.json', 'w') as f:
+    print(json_object, file=f)
